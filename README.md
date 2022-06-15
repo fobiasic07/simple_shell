d37a934593dca723b47871e7822abe653a365647
fobiasic07 <debraobondo@gmail.com>
2022-06-15 17:47:46 +0300

    Betty would be proud

---
+++ README.md
@@ -0,0 +1,2 @@
+This repository is a pair project in the ALX program 2022. It is about building a simple shell with the knowlege attained thus far.
+The files string-to-tokens and puts have been added to test the understaanding of Betty styling when writing code. 
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void _puts(char *str);
+unsigned int nbr_spaces(char *s);
+char **stringToTokens(char *str);
+
+#endif
